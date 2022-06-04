7bdf40c424e727c79821f1cb1eedb3209bc232ed
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-06-04 23:24:22 +0200

    added exercises from part3, section 1

---
+++ part3/1_loops_with_conditions/2_fix_the_code.py
@@ -0,0 +1,8 @@
+# Fix the program
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+counter = number
+while counter > 0:
+    print(counter)
+    counter -= 1
+print("Now!")
---
+++ part3/1_loops_with_conditions/3_numbers.py
@@ -0,0 +1,6 @@
+# Write your solution here
+upperLimit = int(input("Upper limit:"))
+counter = 1
+while counter < upperLimit and counter > 0:
+    print(counter)
+    counter += 1
