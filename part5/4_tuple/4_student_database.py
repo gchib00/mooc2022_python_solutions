121149cac5bb3fdd226df5687308fc1a0e299757
Giorgi Chibukhashvili <chibukhashviligiorgi@gmail.com>
2022-06-27 22:41:19 +0200

    imrpoved the logic of the student database exercise

--- part5/4_tuple/4_student_database.py
+++ part5/4_tuple/4_student_database.py
@@ -3,7 +3,7 @@ def summary(dict: dict):
   # Determine averages:
   averages = []
   for student in dict:
-    average = print_student(dict, student)[0]
+    average = get_summary(dict, student)["average"]
     averages.append((student, average))
   highest_average = 0
   highest_average_student = ""
@@ -14,7 +14,7 @@ def summary(dict: dict):
   # Determine completed courses:
   completed_courses = []
   for student in dict:
-    num_of_completed = print_student(dict, student)[1]
+    num_of_completed = get_summary(dict, student)["total_completed_courses"]
     completed_courses.append((student, num_of_completed))
   highest_completed = 0
   highest_completed_student = ""
@@ -23,44 +23,56 @@ def summary(dict: dict):
       highest_completed = student[1]
       highest_completed_student = student[0]
   # Print summary
+  print("students", len(dict))
   print(f"most courses completed {highest_completed} {highest_completed_student}")
   print(f"best average grade {highest_average} {highest_average_student}")
 def add_course(dict: dict, name: str, course: tuple):
   if name in dict:
     dict[name].append(course)     
 def print_student(dict: dict, name: str):
-  if name in dict:
-    filtered_courses_list = []
-    total_score = 0
-    for course in dict[name]:
-      if course[1] > 0:
-        matches = 0
-        for filtered_course in filtered_courses_list:
-          if course[0] == filtered_course[0]:
-            if course[1] > filtered_course[1]:
-              filtered_courses_list.remove(filtered_course)
-              filtered_courses_list.append(course)
-            matches += 1
-        if matches == 0:
-          filtered_courses_list.append(course)
+  data = get_summary(dict, name)
+  if name in dict and data == None:
     print(name+":")
-    if len(filtered_courses_list) == 0:
-      print(" no completed courses")
-    else:
-      print(f" {len(filtered_courses_list)} completed courses:")
-      for course in filtered_courses_list:
-        print(f"  {course[0]} {course[1]}")
-        total_score += course[1]
-      average = total_score / (len(filtered_courses_list))
-      print(" average grade", average)
-      return (average, len(filtered_courses_list))
+    print(" no completed courses")
+  if data != None:
+    average = data["average"]
+    total_completed_courses = data["total_completed_courses"]
+    completed_courses = data["completed_courses"]
+    print(name+":")
+    print(f" {total_completed_courses} completed courses:")
+    for course in completed_courses:
+      print(f"  {course[0]} {course[1]}")
+    print(" average grade", average)
   else:
-    print(f"{name}: no such person in the database")  
+    if name not in dict:
+      print(f"{name}: no such person in the database")
+    else:
+      print
+def get_summary(dict: dict, name: str):
+    if name in dict:
+      filtered_courses_list = []
+      total_score = 0
+      for course in dict[name]:
+        if course[1] > 0:
+          matches = 0
+          for filtered_course in filtered_courses_list:
+            if course[0] == filtered_course[0]:
+              if course[1] > filtered_course[1]:
+                filtered_courses_list.remove(filtered_course)
+                filtered_courses_list.append(course)
+              matches += 1
+          if matches == 0:
+            filtered_courses_list.append(course)
+      if len(filtered_courses_list) > 0:
+        for course in filtered_courses_list:
+          total_score += course[1]
+        average = total_score / (len(filtered_courses_list))
+        return {
+          "average": average,
+          "total_completed_courses": len(filtered_courses_list),
+          "completed_courses": filtered_courses_list
+        }      
+    else:
+      return None
 def add_student(dict: dict, name: str):
   dict[name] = []
-
-if __name__ == "__main__":
-  students = {}
-  add_student(students, "Peter")
-  add_course(students, "Peter", ("Software Development Methods", 5))
-  summary(students)
